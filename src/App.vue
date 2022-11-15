05892f6bfa630243c4d04da2714eea86e7ae871b
Thinkedou <r.enguehard@gmail.com>
2022-11-15 12:20:31 +0100

    'first'

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {ref} from 'vue'
+import WatchCard from './components/WatchCard.vue';
+
+// on importe notre dataset qui simule un futur call api
+import {userWatchList} from '@/assets/static/js/watchList.js'
+
+const watchList = ref(userWatchList)
+
+console.log(watchList)
+
+</script>
+
+<template>
+  <div class='row'>
+      <div class='col left'>
+          <ul>
+              <li>Ici c'est fixe</li>
+              <li>Parent (nav, button, etc)</li>
+          </ul>
+          <span>▶ Lecture en cours: (ep title) </span>
+         
+      </div>
+      <div class='col right'>
+
+        <WatchCard 
+          v-for="(episode,idx) in watchList"
+          :key="idx"
+          :ep-title="episode.title"
+          :series="episode.series"
+          :episode="episode.episode"
+          :season="episode.season"
+        />
+       
+        
+
+      </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/WatchCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+const props = defineProps({
+    epTitle:{
+        type:String
+    },
+    series:{
+        type:String
+    },
+    episode:{
+        type:Number
+    },
+    season:{
+        type:Number
+    }
+})
+
+
+</script>
+
+<template>
+    <div class="courses-container">
+        <div class="course">
+            <div class="course-preview">
+                <h6>Season {{season}}</h6>
+                <h2>{{series}}</h2>
+            </div>
+            <div class="course-info">
+                <h6>ep: S02E08</h6>
+                <h2>{{epTitle}}</h2>
+                <button class="btn" >Continue</button>
+            </div>
+        </div>  
+    </div>
+</template>
+
+<style>
+</style>
